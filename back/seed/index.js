e58a17464124f84efe97344c0af84faac8f46b76
Nahue941 <nahuelpaivag@gmail.com>
2021-03-10 13:30:48 -0300

    hells chainnig

--- back/seed/index.js
+++ back/seed/index.js
@@ -7,21 +7,61 @@ const itemArray = require("./item");
 const cartArray = require("./cart");
 const pictureArray = require("./picture");
 
+/** Al crear una tabla que contiene un FK. El método bulkCreate revisa que 
+ *  dicho id exista en la tabla a la cual quiero relacionar. Por ende se debe
+ *  controlar el orden en que se crean las tablas. Primero debo crear las que no 
+ *  tienen relación con ningunga otra tabla (en su interior no tienen FK), 
+ *  y luego el resto.
+ *  Orden de creación:
+ *    1-Users
+ *    2-Category
+ * 
+ *    3-Cart
+ *    4-Product
+ * 
+ *    5-Picture
+ *    6-Review
+ *    7-Items
+ *  *El orden no necesariamente debe ser este pero un orden posible.
+ */
+
+User.bulkCreate(userArray)
+  .then(() => {
+    console.log(`-->usuarios creados`);
+    Category.bulkCreate(categoryArray)
+      .then(() => {
+        console.log(`-->categorias creadas`);
+        Cart.bulkCreate(cartArray)
+          .then(() => {
+            console.log(`-->carritos creados`);
+            Product.bulkCreate(productArray)
+              .then(() => {
+                console.log(`-->productos creados`);
+                Picture.bulkCreate(pictureArray)
+                  .then(() => {
+                    console.log(`-->pictures creados`);
+                    Review.bulkCreate(reviewArray)
+                      .then(() => {
+                        console.log(`-->reviews creados`);
+                        Item.bulkCreate(itemArray)
+                          .then(res => {
+                            console.log(`-->items creados`);
+                            return res;
+                          })
+                          .then(() => console.log(`----Seed terminado----`));
+                      });
+                  });
+              });
+          });
+      });
+  });
+
+
+
+/*
 let userPromise = User.bulkCreate(userArray)
   .then(res => {
     console.log(`-->usuarios creados`);
-    return res;
-  });
-
-let productPromise = Product.bulkCreate(productArray)
-  .then(res => {
-    console.log(`-->productos creados`);
-    return res;
-  });
-
-let cartPromise = Cart.bulkCreate(cartArray)
-  .then(res => {
-    console.log(`-->carritos creados`);
     return res;
   });
 
@@ -31,17 +71,17 @@ let categoryPromise = Category.bulkCreate(categoryArray)
     return res;
   });
 
-let reviewPromise = Review.bulkCreate(reviewArray)
+let cartPromise = Cart.bulkCreate(cartArray)
   .then(res => {
-    console.log(`-->reviews creados`);
+    console.log(`-->carritos creados`);
     return res;
   });
 
-/* let itemPromise = Item.bulkCreate(itemArray)
+let productPromise = Product.bulkCreate(productArray)
   .then(res => {
-    console.log(`-->items creados`);
+    console.log(`-->productos creados`);
     return res;
-  }); */
+  });
 
 let picturePromise = Picture.bulkCreate(pictureArray)
   .then(res => {
@@ -49,4 +89,18 @@ let picturePromise = Picture.bulkCreate(pictureArray)
     return res;
   });
 
-Promise.all([userPromise, productPromise, cartPromise, categoryPromise, reviewPromise, picturePromise]).then(() => console.log(`----Seed terminado----`));
+let reviewPromise = Review.bulkCreate(reviewArray)
+  .then(res => {
+    console.log(`-->reviews creados`);
+    return res;
+  });
+
+let itemPromise = Item.bulkCreate(itemArray)
+  .then(res => {
+    console.log(`-->items creados`);
+    return res;
+  });
+
+Promise.all([userPromise, productPromise, cartPromise, categoryPromise, reviewPromise, itemPromise, picturePromise])
+  .then(() => console.log(`----Seed terminado----`));
+*/
