1a7ac4b55e3df3d76f8f3e48d3ba860aa89352cc
PabloMunaretto <pablo.munaretto@gmail.com>
2021-03-10 16:42:20 -0300

    avance singleProduct

--- front/src/components/SingleProduct.jsx
+++ front/src/components/SingleProduct.jsx
@@ -1,8 +1,23 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 // - visual de cada item: (imagen del producto, descripcion del producto, precio, reviews, y valoracion)
 
-const SingleProduct = () => {
-  return <div>SingleProduct</div>;
+import { getProduct } from "../store/singleProduct"
+
+const SingleProduct = ({ productId }) => {
+    const dispatch = useDispatch()
+    const producto = useSelector(state => state.singleProduct)
+
+
+    useEffect(() => {
+      return dispatch(getProduct(productId))
+    }, [])
+
+
+    console.log("producto", producto)
+
+    return (
+    <div>SingleProduct</div>
+    )
 };
 export default SingleProduct;
