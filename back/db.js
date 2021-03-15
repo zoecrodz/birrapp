ecaa5f5eee8797c01caef03dac8d348394a90355
Nahue941 <65076835+Nahue941@users.noreply.github.com>
2021-03-15 11:12:30 -0300

    Mejorando la conexion de la DB
    Se quitó código comentado y se corrigió la conexión. La mayoria del grupo tiene la db sin user ni pass.

--- back/db.js
+++ back/db.js
@@ -1,13 +1,11 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-  "postgres://blinzerd:12345@localhost:5432/birrapp",
+  "postgres:/birrapp",//Conexion con la base de datos sin contraseña ni usario.
+  //"postgres://blinzerd:12345@localhost:5432/birrapp", Conexion WALKER.
   {
     logging: false,
     dialect: "postgres",
-    /* define: {
-    freezeTableName: true //evita que agregue 's' al final del nombre de cada tabla
-  } */
   }
 );
 
