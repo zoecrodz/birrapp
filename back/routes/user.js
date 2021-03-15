9d41e1d9cb47ec399a5009d2de499b4e2902da7c
Nahue941 <nahuelpaivag@gmail.com>
2021-03-15 19:33:07 -0300

    ruta put usuario

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,15 +1,24 @@
-const express = require( 'express' );
+const express = require('express');
 const router = express.Router();
 const { User } = require("../models");
 //por el momento no encontramos un ruteo necesario para users salvo para ver que funciona el register
 
 //users
-router.get("/", (req, res) => {
-    User.findAll()
-    .then(users => res.send(users))
-    .catch(err => console.log(err));
+router.put(`/promote`, (req, res) => {
+
+    User.findByPk(Number(req.body.id))
+        .then(res => res.dataValues)
+        .then(user => {
+            User.update({ admin: !user.admin }, { where: { id: user.id } })
+                .then((user) => res.send(user));
+        })
 })
 
+router.get("/", (req, res) => {
+    User.findAll()
+        .then(users => res.send(users))
+        .catch(err => console.log(err));
+})
 
 
 module.exports = router;
