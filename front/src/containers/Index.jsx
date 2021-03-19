8bed0342e8365512f9b34bd3ec925fa1bb23ac0c
Nahue941 <65076835+Nahue941@users.noreply.github.com>
2021-03-19 11:08:40 -0300

    Persistencia con facebook

--- front/src/containers/Index.jsx
+++ front/src/containers/Index.jsx
@@ -13,15 +13,25 @@ const Index = () => {
   const dispatch = useDispatch()
   const user = useSelector((state) => state.user)
   const carrito = useSelector(state => state.carrito)
-
+  
   useEffect(() => {
-    // preguntar si hay carrito con estado pending y si no, crearlo. con el usuario que tengo, hacer un getCart  
-    dispatch(getUser())
-      .then((usuario) => {
-        if (usuario.payload && !usuario.payload.admin) {
-          if (!carrito.id) dispatch(getCarrito(usuario.payload.id))
-        }
+      // preguntar si hay carrito con estado pending y si no, crearlo. con el usuario que tengo, hacer un getCart
+      if (localStorage.getItem("id")){
+        dispatch(getFbUser(localStorage.getItem("id")))
+        .then((usuario) => {
+          if (usuario.payload && !usuario.payload.admin) {
+            if (!carrito.id) dispatch(getCarrito(usuario.payload.id))
+          }
       })
+    }
+    else {
+      dispatch(getUser())
+        .then((usuario) => {
+          if (usuario.payload && !usuario.payload.admin) {
+            if (!carrito.id) dispatch(getCarrito(usuario.payload.id))
+          }
+        })
+    }
   }, [])
 
   return (
